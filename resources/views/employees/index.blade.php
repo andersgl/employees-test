dfd220f14880ffc37bed7ff256cec3c7af9baeff
Anders Gro Larsen <al@meo.io>
2025-02-20 02:07:11 +0100

    Update employees list

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -5,7 +5,6 @@
                 <table>
                     <thead>
                         <tr>
-                            <th class="px-4 py-2">{{__("ID")}}</th>
                             <th class="px-4 py-2">{{__("Name")}}</th>
                             <th class="px-4 py-2">{{__("Email")}}</th>
                             <th class="px-4 py-2">{{__("Phone")}}</th>
@@ -14,12 +13,12 @@
                             <th class="px-4 py-2">{{__("Salary")}}</th>
                             <th class="px-4 py-2">{{__("Employment From")}}</th>
                             <th class="px-4 py-2">{{__("Employment To")}}</th>
+                            <th class="px-4 py-2">{{__("Currently working")}}</th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($employees as $employee)
                             <tr>
-                                <td class="border px-4 py-2">{{ $employee->id }}</td>
                                 <td class="border px-4 py-2">{{ $employee->firstname }} {{ $employee->lastname }}</td>
                                 <td class="border px-4 py-2">{{ $employee->email }}</td>
                                 <td class="border px-4 py-2">{{ $employee->phone }}</td>
@@ -28,6 +27,7 @@
                                 <td class="border px-4 py-2">{{ $employee->salary }}</td>
                                 <td class="border px-4 py-2">{{ $employee->employment_from }}</td>
                                 <td class="border px-4 py-2">{{ $employee->employment_to }}</td>
+                                <td class="border px-4 py-2">{{ !$employee->isFormerEmployee() ? __("Yes") : __("No") }}</td>
                             </tr>
                         @endforeach
                     </tbody>
